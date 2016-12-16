402fe0b17e00f093669fd608b4a88b62e146a604
Noah Prail <nprail@filiosoft.com>
2016-12-16 16:04:31 -0500

    Initial commit.

---
+++ SetCordovaVersion-CLI/scv.js
@@ -0,0 +1,50 @@
+﻿#!/usr/bin/env node
+var fs = require('fs');
+var program = require('commander');
+var DOMParser = require('xmldom').DOMParser;
+var clc = require('cli-color');
+
+var configfile;
+
+program
+    .version('0.0.1')
+    .arguments('<versionnumber>', 'Version number to be set.')
+    .arguments('<buildnumber>', 'Build number to be set.')
+    .option('-c, --config <config>', 'Location of config.xml. Defaults to ./config.xml. e.g. /path/to/config.xml ')
+    .parse(process.argv);
+
+if (!program.config) {
+    configfile = "config.xml";
+} else {
+    configfile = program.config;
+}
+
+if (fs.existsSync(configfile) && program.versionnumber && program.buildnumber) {
+    fs.readFile(configfile, 'utf-8', function (err, data) {
+        if (err) {
+            throw err;
+        }
+
+        var doc = new DOMParser().parseFromString(data, 'application/xml');
+        console.log('Opening project\'s config.xml file:', clc.green(configfile));
+
+        doc.documentElement.setAttribute('version', program.versionnumber);
+        doc.documentElement.setAttribute('ios-CFBundleVersion', program.buildnumber);
+        doc.documentElement.setAttribute('android-versionCode', program.buildnumber);
+
+        console.log('Setting version number to', clc.green(doc.documentElement.getAttribute('version')));
+        console.log('Setting iOS build number to', clc.green(doc.documentElement.getAttribute('ios-CFBundleVersion')));
+        console.log('Setting Android build number to', clc.green(doc.documentElement.getAttribute('android-versionCode')));
+    });
+}
+else {
+    if (!fs.existsSync(configfile)) {
+        console.log(clc.red(configfile, 'doesn\'t seem to exist. '));
+    }
+    if (!program.versionnumber) {
+        console.log(clc.red('Please specify a version number.'));
+    }
+    if (!program.buildnumber) {
+        console.log(clc.red('Please specify a build number.'));
+    }
+}
